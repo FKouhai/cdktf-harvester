bf522f9bc926a0e7f15d410b0104d002237af636
FKouhai <frandres00@gmail.com>
2025-03-30 19:18:26 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Nix flake to build cdktf-harvester and dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      gomod2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ gomod2nix.overlays.default ];
+          config.allowUnfree = true;
+        };
+        cdktf-harvester = pkgs.buildGoModule {
+          name = "cdktf-harvester";
+          src = ./cdktf-app;
+          vendorHash = "sha256-tf3FwlIRutTc1Ef0zGePS1F5bYSfDakYMt5GFU8XjR4=";
+          runVend = true;
+          doCheck = false;
+          postInstall = ''
+            mv $out/bin/stack $out/bin/cdktf-harvester
+          '';
+        };
+      in
+      with pkgs;
+      {
+        defaultPackage = cdktf-harvester;
+        devShells.default = mkShell {
+          buildInputs = [
+            nodePackages.cdktf-cli
+            go
+            nodejs_20
+            terraform
+          ];
+        };
+      }
+    );
+}
